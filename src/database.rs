9ac24dc41c62a89f42d1d8d833ec6e02cb9944a4
SitiSchu <admin@sitischu.com>
2019-07-11 00:37:27 +0200

    Refactor database.rs
    Signed-off-by: SitiSchu <admin@sitischu.com>

--- src/database.rs
+++ src/database.rs
@@ -1,16 +1,19 @@
 use postgres::{Client, Config, NoTls};
+use slog::Logger;
 
 use crate::settings::Settings;
 use crate::utils;
 
 pub struct Database {
     conn: Client,
+    settings: Settings,
+    logger: Logger,
 }
 
 impl Database {
     pub fn new() -> Database {
-        let logger = utils::logger();
         let settings = Settings::load();
+        let logger = utils::logger();
 
         debug!(logger, "Connecting to database";
          "host" => &settings.database.address,
@@ -27,34 +30,41 @@ impl Database {
             .connect(NoTls).unwrap();
         debug!(logger, "Connected to PostgreSQL");
         Database {
-            conn
+            conn,
+            settings,
+            logger,
         }
     }
 
     pub fn setup_tables(&mut self) {
-        let logger = utils::logger();
         let create_banlist = "CREATE TABLE IF NOT EXISTS banlist (id integer NOT NULL PRIMARY KEY, reason Text NOT NULL, date timestamp NOT NULL);";
-        debug!(logger, "Creating Table"; "query" => create_banlist, "name" => "banlist");
+        debug!(self.logger, "Creating Table"; "query" => create_banlist, "name" => "banlist");
         self.conn.simple_query(create_banlist).unwrap();
 
         let create_tokens = "CREATE TABLE IF NOT EXISTS tokens (id SERIAL, token Text NOT NULL PRIMARY KEY, permissions json NOT NULL, userid integer NOT NULL);";
-        debug!(logger, "Creating Table";"query" => create_tokens,  "name" => "tokens");
+        debug!(self.logger, "Creating Table";"query" => create_tokens,  "name" => "tokens");
         self.conn.simple_query(create_tokens).unwrap();
     }
 
     pub fn create_genesis_token(&mut self) {
         let logger = utils::logger();
-        let settings = Settings::load();
         let get_genesis_token = "SELECT * FROM tokens WHERE id = 1;";
-        debug!(logger, "Checking if Genesis token exists"; "query" => get_genesis_token);
+        debug!(self.logger, "Checking if Genesis Token exists"; "query" => get_genesis_token);
         if self.conn.query(get_genesis_token, &[]).unwrap().is_empty() {
-            info!(logger, "Genesis Token doesn't exist. Creating one"; "size" => settings.token_size);
-            let token = nanoid::generate(settings.token_size as usize);
-            let insert_token = r#"INSERT INTO tokens(token, permissions, userid) VALUES ($1, '{"all": "rw"}', $2);"#;
-            debug!(logger, "Creating Token"; "query" => insert_token);
-            self.conn.execute(insert_token, &[&token, &settings.masterid]).unwrap();
+            info!(self.logger, "Genesis Token doesn't exist. Creating one"; "size" => self.settings.token_size);
+            let token = self.create_token(r#"{"all": "rw"}"#, self.settings.masterid);
+            info!(logger, "Created Genesis Token `{}`. Write this down, this will be the only time you see it.", token.unwrap())
+        } else {
+            debug!(logger, "Genesis Token exists. Skipping creation.")
         }
-        let token: String = self.conn.query(get_genesis_token, &[]).unwrap()[0].get("token");
-        info!(logger, "Found Genesis Token"; "token" => token);
+    }
+
+    pub fn create_token(&mut self, permissions: &str, userid: i32) -> Option<String> {
+        let perms: serde_json::Value = serde_json::from_str(&permissions).unwrap();
+        let token = nanoid::generate(self.settings.token_size as usize);
+        let insert_token = r#"INSERT INTO tokens(token, permissions, userid) VALUES ($1, $2, $3);"#;
+        debug!(self.logger, "Creating Token"; "query" => insert_token);
+        self.conn.execute(insert_token, &[&token, &perms, &userid]).unwrap();
+        Some(token)
     }
 }
