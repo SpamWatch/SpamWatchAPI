c8c523d1f0481f9e29c3bcb6e82c1b902c5ad70b
SitiSchu <admin@sitischu.com>
2020-01-26 12:08:11 +0100

    Add link to documentation
    Signed-off-by: SitiSchu <admin@sitischu.com>

--- src/routes/root.rs
+++ src/routes/root.rs
@@ -3,6 +3,10 @@ use serde_json::json;
 
 use crate::settings;
 
+fn safe_href(name: &str, url: &str) -> String {
+    format!(r#"<a rel="noopener" target="_blank" href="{}" class="white-no-dec-link">{}</a>"#, url, name)
+}
+
 pub fn info() -> HttpResponse {
     let staging_prefix = if settings::ENV.general.staging {
         "<h1 style='color: #DE935F'>Staging Instance. This is not the real API.</h1>"
@@ -13,14 +17,15 @@ pub fn info() -> HttpResponse {
         "
         <style>* {{font-family: monospace;}}</style>
         {}
-        {} v{} by {}<br>{}<br><br><a href={}>GitHub</a><br>
-        <a href=https://t.me/SpamWatch>Channel</a>",
+        {} v{} by {}<br>{}<br><br>{}<br>{}<br>{}",
         staging_prefix,
         &env!("CARGO_PKG_NAME"),
         &env!("CARGO_PKG_VERSION"),
         &env!("CARGO_PKG_AUTHORS"),
         &env!("CARGO_PKG_DESCRIPTION"),
-        &env!("CARGO_PKG_REPOSITORY")
+        safe_href("GitHub", &env!("CARGO_PKG_REPOSITORY")),
+        safe_href("Channel", "https://t.me/SpamWatch"),
+        safe_href("Documentation", "https://docs.spamwat.ch")
     );
     HttpResponse::Ok().content_type("text/html").body(body)
 }
