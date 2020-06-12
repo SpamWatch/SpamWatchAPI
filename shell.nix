b376280bddae0067bca3298d1f32ef2da95d6c23
SitiSchu <admin@sitischu.com>
2020-06-12 04:07:51 +0200

    Fix nix shell postgres error

--- shell.nix
+++ shell.nix
@@ -8,6 +8,8 @@ stdenv.mkDerivation {
     export PGDATA=~/main/db/postgres
     mkdir -p $PGDATA
     initdb
+    sudo mkdir /run/postgresql
+    sudo chown $USER:users /run/postgresql
     pg_ctl -l /tmp/postgres.log start
     tail -f /tmp/postgres.log
   '';
